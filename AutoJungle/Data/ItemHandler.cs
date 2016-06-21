52f5965bf302be5c3d0869bdb2239221d9cc2b45
chienhao10 <huangjianhao_10@hotmail.com>
2016-06-20 21:57:02 -0400

    stkl

--- AutoJungle/Data/ItemHandler.cs
+++ AutoJungle/Data/ItemHandler.cs
@@ -84,7 +84,7 @@ namespace AutoJungle.Data
             ItemList.Add(new BuyingOrder(2, 2031, "Refillable Potion", 150));
             ItemList.Add(new BuyingOrder(3, 3340, "Trinket", 0));
             ItemList.Add(new BuyingOrder(4, 1039, "Hunter's Talisman", 350));
-            ItemList.Add(new BuyingOrder(5, 3715, "Skirmisher's Sabre", 700));
+            ItemList.Add(new BuyingOrder(5, 3706, "Stalker's Blade", 700));
             ItemList.Add(new BuyingOrder(6, 1042, "Dagger", 350));
             ItemList.Add(new BuyingOrder(7, 1419, "Bloodrazor", 1350));
             ItemList.Add(new BuyingOrder(8, (int) ItemId.Boots_of_Speed, "鞋", 475));
